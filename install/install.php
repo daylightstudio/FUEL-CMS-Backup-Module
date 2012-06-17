269079465bc6a0cb6cfc721d055ea3b4b1f9803e
David McReynolds <dave@thedaylightstudio.com>
2012-06-17 09:23:32 -0700

    updated install file

--- install/install.php
+++ install/install.php
@@ -6,13 +6,11 @@ $config['company'] = 'Daylight Studio';
 $config['license'] = 'Apache 2';
 $config['copyright'] = '2012';
 $config['author_url'] = 'http://www.thedaylightstudio.com';
-$config['developer_url'] = 'http://www.thedaylightstudio.com';
 $config['description'] = 'The FUEL Backup Module can be used to backup the FUEL CMS database and assets.';
 $config['compatibility'] = '1.0';
 $config['instructions'] = '';
-//$config['writable'] = ''; // must be relative to the module folder to sandbox
-$config['permissions'] = 'test2';
+$config['permissions'] = 'backup';
 $config['migration_version'] = 1;
 $config['install_sql'] = '';
 $config['uninstall_sql'] = '';
-?>
+$config['repo'] = 'git://github.com/daylightstudio/FUEL-CMS-Backup-Module.git';
