5ccc5ac0c71eb060544e62f8ad00ec136421a7ae
David McReynolds <dave@thedaylightstudio.com>
2013-08-26 16:55:13 -0700

    added ability to call backup/cron if a "webhook_romote_ip" config parameter is set

--- controllers/cron.php
+++ controllers/cron.php
@@ -27,92 +27,104 @@
  */
 
 // --------------------------------------------------------------------
+require_once(FUEL_PATH.'/libraries/Fuel_base_controller.php');
 
-class Cron extends CI_Controller  {
+class Cron extends Fuel_base_controller  {
 	
-	function __construct()
+	public function __construct()
 	{
-		parent::__construct();
-		$this->load->module_library(FUEL_FOLDER, 'fuel');
+		// don't validate initially because we need to handle it a little different since we can use web hooks
+		parent::__construct(FALSE);
+
+		$remote_ips = $this->fuel->config('webhook_romote_ip');
+		$is_web_hook = ($this->fuel->auth->check_valid_ip($remote_ips));
+
+		// check if it is CLI or a web hook otherwise we need to validate
+		$validate = (php_sapi_name() == 'cli' OR defined('STDIN') OR $is_web_hook) ? FALSE : TRUE;
+
+		// validate user has permission
+		if ($validate)
+		{
+			$this->fuel->admin->check_login();
+			$this->_validate_user('backup');
+		}
+
 	}
 	
 	function _remap($method)
 	{
 		// check for CRON OR STDIN constants
-		if (php_sapi_name() == 'cli' OR defined('STDIN') OR defined('CRON'))//add CRON for compatibility with cronjobs module
+        $this->fuel->backup->set_params($this->fuel->backup->config());//set parameters with up to date config/settings...before updating assets flag and download flag....
+		// set assets flag
+		$include_assets = ($method == '1' OR ($method =='index' AND $this->fuel->backup->config('include_assets')));
+		
+		if (!empty($include_assets))
 		{
-            $this->fuel->backup->set_params($this->fuel->backup->config());//set parameters with up to date config/settings...before updating assets flag and download flag....
-			// set assets flag
-			$include_assets = ($method == '1' OR ($method =='index' AND $this->fuel->backup->config('include_assets')));
-			
-			if (!empty($include_assets))
-			{
-				$this->fuel->backup->include_assets = TRUE;
-			}
-			//$this->fuel->backup->allow_overwrite = TRUE;
-			$this->fuel->backup->download = FALSE;
-			
-			// perform backup
-			if (!$this->fuel->backup->do_backup())
-			{
-				$output = $this->fuel->backup->errors(TRUE);
-			}
-			else
-			{
-				$backup_data = $this->fuel->backup->backup_data();
-				$file_name = $backup_data['file_name'];
-				$download_path = $backup_data['full_path'];
-				
-				// set initial output value
-				$output = ($include_assets) ? lang('cron_db_backup_asset', $file_name) : lang('cron_db_backup', $file_name);
-				
-
-				if ($this->fuel->backup->config('cron_email'))
-				{
-					
-					// set parameters for notification
-					$params['to'] = $this->fuel->backup->config('cron_email');
-					$params['message'] = $output;
-					$params['subject'] = lang('cron_email_subject', $this->fuel->config('site_name'));
-					$params['use_dev_mode'] = FALSE; // must be set for emails to always go to what is sent in the backup config
-					if ($this->fuel->backup->config('cron_email_file'))
-					{
-						$params['attachments'] = $download_path;
-					}
-
-					if ($this->fuel->notification->send($params))
-					{
-						$output .= "\n".lang('cron_email', $this->fuel->backup->config('cron_email'));
-					}
-					else
-					{
-						$output .= "\n".lang('cron_email_error', $this->fuel->backup->config('cron_email'));
-					}
-					exit($output);
-				}
-				
-				if ($this->fuel->backup->config('days_to_keep'))
-				{
-					$files = get_dir_file_info($this->fuel->backup->backup_path);
-
-					foreach($files as $file)
-					{
-						$file_date = substr(end(explode($file['name'], '_')), 0, 10);
-						$file_date_ts = strtotime($file['date']);
-						$compare_date = mktime(0, 0, 0, date('m'), date('j') - $this->fuel->backup->config('days_to_keep'), date('Y'));
-						
-						if ($file_date_ts < $compare_date AND strncmp($this->fuel->backup->config('file_prefix'), $file['name'], strlen($this->fuel->backup->config('file_prefix'))) === 0)
-						{
-							@unlink($file['server_path']);
-						}
-					}
-					// log message
-					$msg = lang('data_backup');
-					$this->fuel->logs->write($msg);
-				}
-			}
-			$this->output->set_output($output);
+			$this->fuel->backup->include_assets = TRUE;
 		}
+		//$this->fuel->backup->allow_overwrite = TRUE;
+		$this->fuel->backup->download = FALSE;
+		
+		// perform backup
+		if (!$this->fuel->backup->do_backup())
+		{
+			$output = $this->fuel->backup->errors(TRUE);
+		}
+		else
+		{
+			$backup_data = $this->fuel->backup->backup_data();
+			$file_name = $backup_data['file_name'];
+			$download_path = $backup_data['full_path'];
+			
+			// set initial output value
+			$output = ($include_assets) ? lang('cron_db_backup_asset', $file_name) : lang('cron_db_backup', $file_name);
+			
+
+			if ($this->fuel->backup->config('cron_email'))
+			{
+				
+				// set parameters for notification
+				$params['to'] = $this->fuel->backup->config('cron_email');
+				$params['message'] = $output;
+				$params['subject'] = lang('cron_email_subject', $this->fuel->config('site_name'));
+				$params['use_dev_mode'] = FALSE; // must be set for emails to always go to what is sent in the backup config
+				if ($this->fuel->backup->config('cron_email_file'))
+				{
+					$params['attachments'] = $download_path;
+				}
+
+				if ($this->fuel->notification->send($params))
+				{
+					$output .= "\n".lang('cron_email', $this->fuel->backup->config('cron_email'));
+				}
+				else
+				{
+					$output .= "\n".lang('cron_email_error', $this->fuel->backup->config('cron_email'));
+				}
+				exit($output);
+			}
+			
+			if ($this->fuel->backup->config('days_to_keep'))
+			{
+				$files = get_dir_file_info($this->fuel->backup->backup_path);
+
+				foreach($files as $file)
+				{
+					$file_date = substr(end(explode($file['name'], '_')), 0, 10);
+					$file_date_ts = strtotime($file['date']);
+					$compare_date = mktime(0, 0, 0, date('m'), date('j') - $this->fuel->backup->config('days_to_keep'), date('Y'));
+					
+					if ($file_date_ts < $compare_date AND strncmp($this->fuel->backup->config('file_prefix'), $file['name'], strlen($this->fuel->backup->config('file_prefix'))) === 0)
+					{
+						@unlink($file['server_path']);
+					}
+				}
+				// log message
+				$msg = lang('data_backup');
+				$this->fuel->logs->write($msg);
+			}
+		}
+		$this->output->set_output($output);
 	}
 	
 }
